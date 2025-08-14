09ec2657a523c029d2e8247666d3bf493bb78dca
chenml1 <107552304033@stu.xju.edu.cn>
2025-08-14 18:28:41 +0800

    Update exp_srlane_culane.py

--- configs/exp_srlane_culane.py
+++ configs/exp_srlane_culane.py
@@ -9,8 +9,8 @@ iou_loss_weight = 2.
 cls_loss_weight = 2.
 l1_loss_weight = 0.2
 angle_loss_weight = 5
-attn_loss_weight = 0.  # 0.05
-seg_loss_weight = 0.  # 0.5
+cross_attn_loss_weight = 0. 05 
+seg_loss_weight = 0. 5 
 
 total_iter = 44440
 batch_size = 40
@@ -23,3 +23,4 @@ precision = "16-mixed"  # "32"
 optimizer = dict(type="AdamW", lr=6e-4)
 
 scheduler = dict(type="warmup", warm_up_iters=800, total_iters=total_iter)
+
